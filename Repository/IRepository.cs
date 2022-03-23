8e8b8aa099a416c38740ec60947c1fb9dbf8b26d
Ali <2085836@cognizant.com>
2022-03-23 14:40:59 +0530

    Authorization Microservice

---
+++ Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Authorization.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Authorization.Repository
+{
+    public interface IRepository
+    {
+        
+      
+
+       public string GenerateJSONWebToken(IConfiguration _congig,Member memberDetail);
+    }
+}
